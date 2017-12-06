31f2734e83a62599c93841ddbb532147edbd13d5
mika <mika@popeyedong-macbook-pro.local>
2017-12-06 15:35:43 +0800

    Initial Commit

---
+++ MikaCalendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MikaCalendar
+//
+//  Created by mika on 2017/12/6.
+//  Copyright © 2017年 mika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MikaCalendar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MikaCalendar
+//
+//  Created by mika on 2017/12/6.
+//  Copyright © 2017年 mika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
